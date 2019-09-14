927dacf99e3d0b430cf68c0b56088a55b6b6a8f3
Patryk Ptasinski <patryk@ipepe.pl>
2019-09-14 18:05:12 +0200

    Dodalem cigabutt do minigry1

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # pm-game
 HackYeah 2019 Game
-
-# image credits
- * https://www.vectorstock.com/royalty-free-vector/cigarette-butt-cartoon-character-vector-8737423
--- javascripts/initialStates.js
+++ javascripts/initialStates.js
@@ -7,11 +7,8 @@ window.bootState = {
 
 window.loadState= {
   preload: function() {
-    var loadingLabel = game.add.text(80, 150, 'loading...',
-      {font: '30px Courier', fill: '#ffffff'});
-    game.load.image('player', '/files/theme/player.png');
-    game.load.image('win', '/files/theme/win.png');
-
+    game.add.text(80, 150, 'loading...', { fill: '#ffffff'});
+    game.load.image('cigabutt', 'assets/cigabutt_96x200.png');
   },
 
   create: function() {
